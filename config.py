41eb09e49e994848def67dc6ed9a61cc2173aa11
Rashidsultan1312 <rashidsuk1io@gmail.com>
2026-03-31 13:57:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ webview_power_state = "on"       # "on" / "off" — перед ревью Apple 
 use_hideclick = False       # "True" / "False" — включить или выключить фильтр
 
 # Оффер
-offer_url = "https://www.google.com/"
+offer_url = "https://zumpwick.com/PzpG7Q"
 
 # HideClick API
 hideclick_api_key = "v1a80428f72ca04f3f9348b2319ad04338"
